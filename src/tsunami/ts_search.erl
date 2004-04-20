7b8e6c4e5588f2e891cdb919efeb542880743f71
Nicolas Niclausse <nicolas@niclux.org>
2004-04-20 16:57:24 +0000

    - check return value of function - forgot to add DynVar in some cases
    SVN Revision: 313

--- src/tsunami/ts_search.erl
+++ src/tsunami/ts_search.erl
@@ -47,17 +47,18 @@ subst([], DynVar, Acc) ->
 subst([$%,$%,$_|Rest], DynVar, Acc) ->
     extract_variable(Rest, DynVar, Acc, []);
 subst([$%,$%|Rest], DynVar, Acc) ->
-    extract_module(Rest, Acc, []);
+    extract_module(Rest,  DynVar, Acc, []);
 subst([H|Tail], DynVar, Acc) ->
     subst(Tail, DynVar, [H|Acc]).
 
 %% Search for the module string in the subst markup
-extract_module([],Acc,Mod) ->
+extract_module([],DynVar, Acc,Mod) ->
     lists:reverse(Acc);
-extract_module([$:|Tail],Acc,Mod) ->
-    extract_function(Tail,Acc,lists:reverse(Mod),[]);
-extract_module([H|Tail],Acc,Mod) ->
-    extract_module(Tail,Acc,[H|Mod]).
+extract_module([$:|Tail],DynVar, Acc,Mod) ->
+    ?DebugF("found module name: ~p~n",[lists:reverse(Mod)]),
+    extract_function(Tail,DynVar, Acc,lists:reverse(Mod),[]);
+extract_module([H|Tail],DynVar, Acc,Mod) ->
+    extract_module(Tail,DynVar, Acc,[H|Mod]).
 
 %% Search for the module string in the subst markup
 extract_variable([],DynVar,Acc,Var) ->
@@ -68,6 +69,7 @@ extract_variable([$%,$%|Tail], DynVar, Acc, Var) ->
     VarName = list_to_atom(lists:reverse(Var)),
     case lists:keysearch(VarName,1,DynVar) of 
         {value, {VarName, Result}} ->
+            ?DebugF("found value ~p for name ~p~n",[Result,VarName]),
             subst(Tail, DynVar,lists:reverse(Result) ++ Acc);
         _ ->
             ?LOGF("DynVar: no value found for var ~p~n",[VarName],?WARN),
@@ -79,15 +81,25 @@ extract_variable([H|Tail],DynVar,Acc,Mod) ->
 
 %% Search for the function string and do the real substitution before
 %% keeping on the parsing
-extract_function([], Acc, Mod, Fun) ->
+extract_function([], DynVar, Acc, Mod, Fun) ->
     lists:reverse(Acc);    
-extract_function([$%,$%|Tail], Acc, Mod, Fun) ->
+extract_function([$%,$%|Tail], DynVar, Acc, Mod, Fun) ->
+    ?DebugF("found function name: ~p~n",[lists:reverse(Fun)]),
     Module = list_to_atom(Mod),
     Function = list_to_atom(lists:reverse(Fun)),
-    Result = Module:Function(self()),
-    subst(Tail, lists:reverse(Result) ++ Acc);
-extract_function([H|Tail], Acc, Mod, Fun) ->
-    extract_function(Tail, Acc, Mod, [H|Fun]).
+    Result = case Module:Function(self()) of 
+                 Int when is_integer(Int) ->
+                     lists:reverse(integer_to_list(Int));
+                 Str when is_list(Str) ->
+                     Str;
+                 _Val ->
+                     ?LOGF("extract fun:bad result ~p~n",[_Val],?WARN),
+                     []
+             end,
+    subst(Tail, DynVar, lists:reverse(Result) ++ Acc);
+        
+extract_function([H|Tail],DynVar,  Acc, Mod, Fun) ->
+    extract_function(Tail, DynVar, Acc, Mod, [H|Fun]).
 
 %%----------------------------------------------------------------------
 %% Func: match/2 
