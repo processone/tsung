681df4da295d73d077eb2a918a6515ab6fe1f66b
Nicolas Niclausse <nicolas@niclux.org>
2005-07-19 22:16:03 +0000

    new: we can bench an HTTP proxy, with http_use_server_as_proxy option.
    SVN Revision: 490

--- src/tsunami_controller/ts_config_http.erl
+++ src/tsunami_controller/ts_config_http.erl
@@ -54,7 +54,10 @@ parse_config(Element = #xmlElement{name=http},
     Version  = ts_config:getAttr(Element#xmlElement.attributes, version),
     URL      = ts_config:getAttr(Element#xmlElement.attributes, url),
     Contents = ts_config:getAttr(Element#xmlElement.attributes, contents),
-    UseProxy = ts_config:getAttr(atom, Element#xmlElement.attributes, use_server_as_proxy, false),
+    UseProxy = case ets:lookup(Tab,{http_use_server_as_proxy}) of 
+                   [] -> false;
+                   _ -> true
+               end,
     %% Apache Tomcat applications need content-type informations to read post forms
     ContentType = ts_config:getAttr(string,Element#xmlElement.attributes,
                             content_type, "application/x-www-form-urlencoded"),
@@ -84,10 +87,7 @@ parse_config(Element = #xmlElement{name=http},
                    _ -> 
                        Request
                end,
-    PreviousHTTPServer = case ets:lookup(Tab, {http_server, CurS#session.id}) of 
-                             [] -> [];
-                             [{_Key,PrevServ}] -> PrevServ
-                         end,
+    PreviousHTTPServer = get_previous_http_server(Tab, CurS#session.id),
     Msg = case lists:keysearch(www_authenticate,#xmlElement.name,
                                Element#xmlElement.content) of
               {value, AuthEl=#xmlElement{} } ->
@@ -112,7 +112,10 @@ parse_config(Element = #xmlElement{name=default}, Conf = #config{session_tab = T
     case ts_config:getAttr(Element#xmlElement.attributes, name) of
         "user_agent" ->
             Val = ts_config:getAttr(Element#xmlElement.attributes, value), %FIXME: useless
-            lists:foldl( fun(A,B)->parse_config(A,B) end, Conf, Element#xmlElement.content)
+            lists:foldl( fun(A,B)->parse_config(A,B) end, Conf, Element#xmlElement.content);
+        "http_use_server_as_proxy" ->
+            Val = ts_config:getAttr(Element#xmlElement.attributes, value),
+            ets:insert(Tab,{{http_use_server_as_proxy}, Val})
     end,
     lists:foldl( fun(A,B)->ts_config:parse(A,B) end, Conf, Element#xmlElement.content);
 %% Parsing user_agent
@@ -135,6 +138,11 @@ parse_config(Element = #xmlElement{}, Conf = #config{}) ->
 parse_config(_, Conf = #config{}) ->
     Conf.
 
+get_previous_http_server(Ets, Id) ->
+    case ets:lookup(Ets, {http_server, Id}) of 
+        [] -> [];
+        [{_Key,PrevServ}] -> PrevServ
+    end.
 
 %%----------------------------------------------------------------------
 %% Func: set_msg/3
@@ -152,24 +160,25 @@ set_msg(HTTP=#http_request{url="http" ++ URL},
     HostHeader = set_host_header(URLrec),
     Port = set_port(URLrec),
     Scheme = set_scheme(URLrec#url.scheme),
-    ets:insert(Tab,{{http_server, Id}, {HostHeader}}),
-    set_msg2(HTTP#http_request{url=Path, host_header = HostHeader}, ThinkTime,
-            #ts_request{ack  = parse,
-						subst = SubstFlag,
-						match = MatchRegExp,
-                        host = URLrec#url.host,
-                        scheme = Scheme,
-                        port = Port});
+    ets:insert(Tab,{{http_server, Id}, {HostHeader, URLrec#url.scheme}}),
+
+    {RealServer, RealPath} = case UseProxy of
+                     true  -> {Server, "http"++ URL};
+                     false -> {#server{host=URLrec#url.host, port=Port, type=Scheme},
+                               Path}
+                 end,
+                                 
+    set_msg2(HTTP#http_request{url=RealPath, host_header = HostHeader}, ThinkTime,
+             #ts_request{ack    = parse,
+                         subst  = SubstFlag,
+                         match  = MatchRegExp,
+                         host   = RealServer#server.host,
+                         scheme = RealServer#server.type,
+                         port   = RealServer#server.port});
 
 %% relative URL, no previous HTTP server (hence, first request in session)
 set_msg(HTTPRequest, Think, {SubstFlag, MatchRegExp, false, Server, [],_Tab,_Id}) -> 
-    Tmp= case Server#server.type of
-             gen_tcp -> "http";
-             ssl -> "https"
-         end,
-    URL =lists:append([Tmp, "://", Server#server.host, ":",
-                      integer_to_list(Server#server.port),
-                      HTTPRequest#http_request.url]),
+    URL = server_to_url(Server) ++ HTTPRequest#http_request.url,
     set_msg(HTTPRequest#http_request{url=URL}, Think, {SubstFlag, MatchRegExp, false, Server, [],_Tab,_Id});
     
 %% relative URL, no previous HTTP server, use proxy, error !
@@ -177,18 +186,16 @@ set_msg(HTTPRequest, Think, {SubstFlag, MatchRegExp, true, _Server, [],_Tab,_Id}
     ?LOG("Need absolut URL when using a proxy ! Abort",?ERR),
     throw({error, badurl_proxy});
 %% relative URL, no proxy
-set_msg(HTTPRequest, Think, {SubstFlag, MatchRegExp, false, _Server, {HostHeader},_Tab,_Id}) -> 
-%%% use global host, port and scheme (undefined value), it will be
-%%% dynamicaly set during the run (using default server or previous
-%%% one used in the current session)
+set_msg(HTTPRequest, Think, {SubstFlag, MatchRegExp, false, _Server, {HostHeader,_},_Tab,_Id}) -> 
+    %% don't set host, port and scheme (undefined value), it will be
+    %% dynamicaly set during the run (using default server or previous
+    %% one used in the current session)
     set_msg2(HTTPRequest#http_request{host_header= HostHeader}, Think,
              #ts_request{ack = parse, subst = SubstFlag, match = MatchRegExp });
-set_msg(HTTPRequest, Think, {SubstFlag, MatchRegExp, true, Server, {HostHeader},_Tab,_Id}) -> % relative URL, use proxy
-%%% use global host, port and scheme (undefined value), it will be
-%%% dynamicaly set during the run (using default server or previous
-%%% one used in the current session)
-    set_msg2(HTTPRequest#http_request{host_header= HostHeader}, Think,
-             #ts_request{ack = parse, subst = SubstFlag, match = MatchRegExp }).
+set_msg(HTTPRequest, Think, {SubstFlag, MatchRegExp, true, Server, {HostHeader, PrevScheme},Tab,Id}) -> % relative URL, use proxy
+    %% set absolut URL using previous Server used.
+    URL = atom_to_list(PrevScheme) ++ "://" ++ HostHeader ++ HTTPRequest#http_request.url,
+    set_msg(HTTPRequest#http_request{url=URL}, Think, {SubstFlag, MatchRegExp, true, Server, {HostHeader, PrevScheme},Tab,Id}).
 
 %% Func: set_mgs2/3
 %% Purpose: set param and thinktime in ts_request
@@ -200,6 +207,15 @@ set_msg2(HTTPRequest, Think, Msg) -> % end of a page, wait before the next one
 	Msg#ts_request{ endpage   = true,
                     thinktime = Think,
                     param = HTTPRequest }.
+
+server_to_url(Server=#server{port=80, host= Host, type= gen_tcp})->
+    "http://" ++ Host;
+server_to_url(Server=#server{port=Port, host= Host, type= gen_tcp})->
+    "http://" ++ Host ++ ":" ++ integer_to_list(Server#server.port);
+server_to_url(Server=#server{port=443, host= Host, type= ssl})->
+    "https://" ++ Host;
+server_to_url(Server=#server{port=Port, host= Host, type= ssl})->
+    "https://" ++ Host ++ ":" ++ integer_to_list(Server#server.port).
 
 %%--------------------------------------------------------------------
 %% Func: set_host_header/1
