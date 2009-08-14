3c807fa249bd3c7505b1b5387ebb70eacbb54251
Nicolas Niclausse <nicolas@niclux.org>
2009-08-14 08:17:12 +0000

    Remove MUC extension in join because it's not compliant (Pablo Polvorin, see TSUN-107 for more details)
    SVN Revision: 1042

--- src/tsung/ts_jabber_common.erl
+++ src/tsung/ts_jabber_common.erl
@@ -518,7 +518,6 @@ publish_pubsub_node(Domain, PubSubComponent, Username, Node, Size) ->
 
 muc_join(Room,Nick, Service) ->
     Result = list_to_binary(["<presence to='", Room,"@", Service,"/", Nick, "'>",
-                             "<x xmlns='http://jabber.org/protocol/muc'/>",
                              " </presence>"]),
     Result.
 
