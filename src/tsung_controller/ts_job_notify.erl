03ffe8005aded41a411f5ef0cfbbff2183f4e6b2
Nicolas Niclausse <nicolas@niclux.org>
2011-05-06 14:07:42 +0200

    compilation fix

--- src/tsung_controller/ts_job_notify.erl
+++ src/tsung_controller/ts_job_notify.erl
@@ -160,7 +160,7 @@ handle_cast({wait_jobs, Pid}, State=#state{jobs=Jobs}) ->
     {noreply, State};
 
 handle_cast({listen, undefined}, State) ->
-    ?LOGF("No listen port defined, can't open listening socket ~p~n",[Name],?ERR),
+    ?LOG("No listen port defined, can't open listening socket ~n",?ERR),
     {noreply, State};
 handle_cast({listen,Port}, State) ->
     Opts = [{reuseaddr, true}, {active, once}],
