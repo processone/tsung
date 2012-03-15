6f17308a85bf049fbc1ff6c3a0da7d569263631a
Nicolas Niclausse <nicolas@niclux.org>
2012-03-15 10:09:22 +0100

    R15B fix: use list_to_binary instead of concat_binary

--- src/lib/pgsql_proto.erl
+++ src/lib/pgsql_proto.erl
@@ -604,7 +604,7 @@ encode_message(bind, Bind={NamePortal, NamePrepared,
                                      fun (Bin) when is_binary(Bin) -> <<1:16/integer>>;
                                          (Text) -> <<0:16/integer>> end,
                                      Parameters),
-                {NParameters, erlang:concat_binary(ParamFormatsList)}
+                {NParameters, erlang:list_to_binary(ParamFormatsList)}
     end,
 
     ParametersList = lists:map(
@@ -625,14 +625,14 @@ encode_message(bind, Bind={NamePortal, NamePrepared,
                                <<Size:32/integer, Bin/binary>>
                        end,
                        Parameters),
-    ParametersP = erlang:concat_binary(ParametersList),
+    ParametersP = erlang:list_to_binary(ParametersList),
 
     NResultFormats = length(ResultFormats),
     ResultFormatsList = lists:map(
                           fun (binary) -> <<1:16/integer>>;
                               (text) ->   <<0:16/integer>> end,
                           ResultFormats),
-    ResultFormatsP = erlang:concat_binary(ResultFormatsList),
+    ResultFormatsP = erlang:list_to_binary(ResultFormatsList),
 
     %%io:format("encode bind: ~p~n", [{PortalP, PreparedP,
     %%			     NParameters, ParamFormatsP,
