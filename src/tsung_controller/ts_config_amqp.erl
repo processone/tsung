b0ef58091067b4a20778b3c28c441a8f10ea0076
onlychoice <jzhihui521@gmail.com>
2013-04-07 11:45:40 +0800

    add amqp support

---
+++ src/tsung_controller/ts_config_amqp.erl
@@ -0,0 +1,145 @@
+%%%  This program is free software; you can redistribute it and/or modify
+%%%  it under the terms of the GNU General Public License as published by
+%%%  the Free Software Foundation; either version 2 of the License, or
+%%%  (at your option) any later version.
+%%%
+%%%  This program is distributed in the hope that it will be useful,
+%%%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%%  GNU General Public License for more details.
+%%%
+%%%  You should have received a copy of the GNU General Public License
+%%%  along with this program; if not, write to the Free Software
+%%%  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+%%%
+%%%  In addition, as a special exception, you have the permission to
+%%%  link the code of this program with any library released under
+%%%  the EPL license and distribute linked combinations including
+%%%  the two.
+-module(ts_config_amqp).
+
+-vc('$Id$ ').
+-author('jzhihui521@gmail.com').
+
+-export([parse_config/2]).
+
+-include("ts_profile.hrl").
+-include("ts_config.hrl").
+-include("ts_amqp.hrl").
+
+-include("xmerl.hrl").
+
+%%----------------------------------------------------------------------
+%% Func: parse_config/2
+%% Args: Element, Config
+%% Returns: List
+%% Purpose: parse a request defined in the XML config file
+%%----------------------------------------------------------------------
+%% Parsing other elements
+parse_config(Element = #xmlElement{name = dyn_variable}, Conf = #config{}) ->
+    ts_config:parse(Element, Conf);
+parse_config(Element = #xmlElement{name = amqp},
+             Config = #config{curid = Id, session_tab = Tab,
+                              sessions = [CurS | _], dynvar = DynVar,
+                              subst = SubstFlag, match = MatchRegExp}) ->
+    TypeStr = ts_config:getAttr(string, Element#xmlElement.attributes, type),
+    Type = list_to_atom(TypeStr),
+
+    ReqList = case Type of
+        %% connection.open request, we add all the requests to be done
+        'connection.open' ->
+            ['connect', 'connection.start_ok', 'connection.tune_ok',
+             'connection.open', 'channel.open'];
+        _ ->
+            [Type]
+    end,
+    ResultId = lists:foldl(fun(RequestType, CurrId) ->
+                {Ack, Request} = parse_request(Element, RequestType, Tab),
+                Msg = #ts_request{ack = Ack,
+                                  endpage = true,
+                                  dynvar_specs  = DynVar,
+                                  subst   = SubstFlag,
+                                  match   = MatchRegExp,
+                                  param   = Request},
+                ets:insert(Tab, {{CurS#session.id, CurrId}, Msg}),
+                CurrId + 1
+        end, Id, ReqList),
+    
+    ts_config:mark_prev_req(Id - 1, Tab, CurS),
+    lists:foldl(fun(A, B) -> ts_config:parse(A, B) end,
+                Config#config{dynvar = [], curid = ResultId - 1},
+                Element#xmlElement.content);
+
+%% Parsing options
+parse_config(Element = #xmlElement{name=option}, Conf = #config{session_tab = Tab}) ->
+    NewConf = case ts_config:getAttr(Element#xmlElement.attributes, name) of
+        "username" ->
+            Val = ts_config:getAttr(string,Element#xmlElement.attributes,
+                                    value,?AMQP_USER),
+            ets:insert(Tab,{{amqp_username,value}, Val}),
+            Conf;
+        "password" ->
+            Val = ts_config:getAttr(string,Element#xmlElement.attributes,
+                                    value,?AMQP_PASSWORD),
+            ets:insert(Tab,{{amqp_password,value}, Val}),
+            Conf
+    end,
+    lists:foldl(fun(A,B) -> ts_config:parse(A,B) end,
+                NewConf, Element#xmlElement.content);
+
+%% Parsing other elements
+parse_config(Element = #xmlElement{}, Conf = #config{}) ->
+    ts_config:parse(Element,Conf);
+%% Parsing non #xmlElement elements
+parse_config(_, Conf = #config{}) ->
+    Conf.
+
+parse_request(Element, Type = 'connection.open', _Tab) ->
+    Vhost = ts_config:getAttr(string, Element#xmlElement.attributes, vhost, "/"),
+    Request = #amqp_request{type = Type, vhost = Vhost},
+    {parse, Request};
+parse_request(Element, Type = 'basic.publish', _Tab) ->
+    Exchange = ts_config:getAttr(string, Element#xmlElement.attributes,
+                                 exchange, ""),
+    RoutingKey = ts_config:getAttr(string, Element#xmlElement.attributes,
+                                   routing_key, "/"),
+    Size = ts_config:getAttr(float_or_integer, Element#xmlElement.attributes,
+                             size, 100),
+    PersistentStr = ts_config:getAttr(string, Element#xmlElement.attributes,
+                                      persistent, "false"),
+    Persistent = list_to_atom(PersistentStr),
+    Request = #amqp_request{type = Type, exchange = Exchange,
+                            routing_key = RoutingKey, size = Size,
+                            persistent = Persistent},
+    {no_ack, Request};
+parse_request(Element, Type = 'basic.consume', _Tab) ->
+    Queue = ts_config:getAttr(string,
+                              Element#xmlElement.attributes, queue, ""),
+    AckStr = ts_config:getAttr(string,
+                              Element#xmlElement.attributes, ack, "false"),
+    Ack = list_to_atom(AckStr),
+    Request = #amqp_request{type = Type, queue = Queue, ack = Ack},
+    {parse, Request};
+parse_request(Element, Type = 'basic.qos', _Tab) ->
+    PrefetchSize = ts_config:getAttr(float_or_integer,
+                              Element#xmlElement.attributes, prefetch_size, 0),
+    PrefetchCount = ts_config:getAttr(float_or_integer,
+                              Element#xmlElement.attributes, prefetch_count, 0),
+    Request = #amqp_request{type = Type, prefetch_size = PrefetchSize,
+                            prefetch_count = PrefetchCount},
+    {parse, Request};
+parse_request(_Element, Type = 'connection.start_ok', Tab) ->
+    UserName = ts_config:get_default(Tab, amqp_username),
+    Password = ts_config:get_default(Tab, amqp_password),
+    Request = #amqp_request{type = Type, username = UserName,
+                            password = Password},
+    {parse, Request};
+parse_request(_Element, Type = 'connection.tune_ok', _Tab) ->
+    Request = #amqp_request{type = Type},
+    {no_ack, Request};
+parse_request(_Element, Type = 'confirm.select', _Tab) ->
+    Request = #amqp_request{type = Type},
+    {parse, Request};
+parse_request(_Element, Type, _Tab) ->
+    Request = #amqp_request{type = Type},
+    {parse, Request}.
